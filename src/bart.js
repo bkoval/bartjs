b3d096208335892075320714c756d59be35ec079
bkoval <brt.kowal@gmail.com>
2014-02-28 00:20:40 +0100

    Adding arguments check

--- src/bart.js
+++ src/bart.js
@@ -1,14 +1,25 @@
 var b = ( function() {
 
+	//Exceptions
+	function InvalidArgumentException() {
+   		this.message = 'Illegal Type Of Argument Passed To The Function';
+   		this.name = "IllegalArgumentException";
+	}
+
 	//Holder of the event subscriptions
 	var events = {};
 
 	//Subscribe for a custom event
 	function on ( event, handler ) {
+		if( typeof event !== 'string' || typeof handler !== 'function' ){
+			throw new InvalidArgumentException();
+		}
 		if ( !events[event] ) {
 			events[event] = [];
 		}
-		events[event].push( handler );
+		if ( typeof handler === 'function' ) {
+			events[event].push( handler );
+		}
 	}
 
 	//Emit a custom event to all subscribers
